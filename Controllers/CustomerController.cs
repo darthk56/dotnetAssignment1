77685eef4cb7fd31abfc56e26b2f6b6d0ab49eaa
darthk5656 <darthk5656@gmail.com>
2024-03-11 10:16:46 -0500

    midterm finished

--- Controllers/CustomerController.cs
+++ Controllers/CustomerController.cs
@@ -1,29 +1,31 @@
 using Microsoft.AspNetCore.Mvc;
 
 
+
 namespace Northwind.Controllers
 {
     public class CustomerController(DataContext db) : Controller
     {
         // this controller depends on the NorthwindRepository
         private readonly DataContext _dataContext = db;
-                public IActionResult Register() => View(new Customer());
-                //   [HttpPost]
-                //   [ValidateAntiForgeryToken]
-                // public IActionResult Register(string name){
-                //     if (ModelState.IsValid)
-                //     {
-                //     if (_dataContext.Customers.Any(c => c.CompanyName == model.CompanyName))
-                //     {
-                //         ModelState.AddModelError("", "Name must be unique");
-                //     }
-                //     else
-                //     {
-                //         _dataContext.Register(model);
-                //         return RedirectToAction("Index");
-                //     }
-                //     }
-                //     return View();
-                // }
+        public IActionResult Register() => View(new Customer());
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Register(Customer model){
+            if (ModelState.IsValid)
+            {
+            if (_dataContext.Customers.Any(c => c.CompanyName == model.CompanyName))
+            {
+                ModelState.AddModelError("", "Name must be unique");
+            }
+            else
+            {
+                _dataContext.Register(model);
+                return RedirectToAction("Index");
+            }
+            }
+            return View(new Customer());
+        }
+        public IActionResult Index() => View(_dataContext.Customers);
     }
 }
---
+++ Migrations/20240311142405_CreateCustomer2.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace dotnetAssignment1.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateCustomer2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Address",
+                table: "Customers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "City",
+                table: "Customers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Country",
+                table: "Customers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Fax",
+                table: "Customers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Phone",
+                table: "Customers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "PostalCode",
+                table: "Customers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Region",
+                table: "Customers",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Address",
+                table: "Customers");
+
+            migrationBuilder.DropColumn(
+                name: "City",
+                table: "Customers");
+
+            migrationBuilder.DropColumn(
+                name: "Country",
+                table: "Customers");
+
+            migrationBuilder.DropColumn(
+                name: "Fax",
+                table: "Customers");
+
+            migrationBuilder.DropColumn(
+                name: "Phone",
+                table: "Customers");
+
+            migrationBuilder.DropColumn(
+                name: "PostalCode",
+                table: "Customers");
+
+            migrationBuilder.DropColumn(
+                name: "Region",
+                table: "Customers");
+        }
+    }
+}
